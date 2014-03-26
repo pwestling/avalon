6f5e0de48b7c9086fff8e15b15702e08ff00265e
Armaan Sarkar <asarkar@rapleaf.com>
2014-03-26 00:11:49 -0700

    adds basic boot function to server

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject avalon "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 ])
+  :dependencies
+    [[org.clojure/clojure "1.5.1"]
+     [ring/ring-jetty-adapter "1.1.6"]]
+  :dev-dependencies
+    [[ring/ring-devel "0.2.0"]])
--- src/avalon/server.clj
+++ src/avalon/server.clj
@@ -1,6 +1,10 @@
-(ns avalon.server)
+(ns avalon.server
+  (:use ring.adapter.jetty))
 
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/plain"}
    :body "Hello World!"})
+
+(defn boot [port]
+  (run-jetty handler {:port port}))
