365a3be68f2844e83bbd1cc5b7e51caba42ff5f7
Armaan Sarkar <asarkar@rapleaf.com>
2014-04-08 00:18:48 -0700

    adds user auth layer

--- src/avalon/controllers/login.clj
+++ src/avalon/controllers/login.clj
@@ -1,14 +1,19 @@
 (ns avalon.controllers.login
   (:use compojure.core ring.util.response)
-  (:require [avalon.views.login :as view]))
+  (:require [avalon.views.login :as view]
+            [avalon.models.user :as user-model]))
 
 (defn update-name [name])
 
 (defn index []
   (view/index))
 
+(defn store-user [request user-id]
+  (update-in request [:cookies "user-id"] (fn [i] user-id)))
+
 (defn create [name request]
   (let
-    [origin (get (:headers request) "origin")]
-    (update-name name)
-    (redirect origin)))
+    [origin (get (:headers request) "origin")
+     user-id (user-model/new-user { :name name })
+     redirected-response (redirect origin)]
+    (store-user redirected-response user-id)))
--- src/avalon/middleware/auth.clj
+++ src/avalon/middleware/auth.clj
@@ -1,12 +1,22 @@
 (ns avalon.middleware.auth
-  (:use compojure.core ring.util.response))
+  (:use compojure.core ring.util.response)
+  (:require [avalon.models.user :as user-model]
+            [avalon.db :as db]))
 
 (defn logged-in? [request]
-  true)
+  (let
+    [user-id (get (:cookies request) "user-id")]
+    (and
+      (not (nil? user-id))
+      (user-model/exists? user-id))))
 
 (defn with-auth [handler]
   (fn [request]
     (let [login-path "/login"]
       (if (logged-in? request)
-        (handler request)
+        (let
+          [user-id (:value (get (:cookies request) "user-id")) ;why am i getting the :value?
+           user (db/get-entry "user" user-id)
+           request-with-user (update-in request [:session "user"] (fn [i] (do user)))] ;there must be a better way to update the session
+          (handler request-with-user))
         (redirect login-path)))))
