137a6ec0299a61c07deace144d74078b5efa9cfc
Armaan Sarkar <asarkar@rapleaf.com>
2014-04-12 10:16:00 -0700

    moves create button to below the list

--- src/avalon/views/home.clj
+++ src/avalon/views/home.clj
@@ -5,10 +5,10 @@
   (layout
     [:div
       [:div { :class "list-group" }
-        [:a { :class "list-group-item active" :href "/new" :style "text-align:center" } "Start a new game"]]
-      [:div { :class "list-group" }
       [:div { :class "list-group-item" :style "text-align:center; " } "Open Games"]
       (for [game current-games]
         [:a { :class "list-group-item" :href (str "/game/" (game :name)) }
           (game :name)
-          [:div { :class "badge" } (count (:players game))]])]]))
+          [:div { :class "badge" } (count (:players game))]])]
+          [:div { :class "list-group" }
+            [:a { :class "list-group-item active" :href "/new" :style "text-align:center" } "Start a new game"]]]))
