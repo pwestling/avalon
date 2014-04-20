90d2cfab6dc6d3ecd7ad352a2c5af5f402193598
Armaan Sarkar <asarkar@rapleaf.com>
2014-04-20 13:42:10 -0700

    remove dummy games

--- src/avalon/models/game.clj
+++ src/avalon/models/game.clj
@@ -3,16 +3,6 @@
             [avalon.models.user :as user]
             [avalon.core :as avalon]))
 
-(def GAME-PROPOSAL
-  {:name "LiveRamp"
-   :stage :propose
-   :players [["Takashi" 2] ["Chris" 3] ["Porter" 4] ["Jacob" 1]]})
-
-(def GAME-VOTE
-  {:name "LiveRamp"
-   :stage :vote
-   :team [["Takashi" 2] ["Chris" 3]]})
-
 (defn all []
   (db/get-all "game"))
 
