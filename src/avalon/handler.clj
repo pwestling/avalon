7eba7d7b51e47b8114672ffd1cb34fe554000e7e
Armaan Sarkar <asarkar@rapleaf.com>
2014-04-20 16:13:29 -0700

    organizes routes

--- src/avalon/handler.clj
+++ src/avalon/handler.clj
@@ -8,15 +8,16 @@
             [avalon.controllers.login :as login]
             [avalon.middleware.auth :as auth]))
 
-(defroutes auth-routes
+(defroutes resource-routes
   (GET "/" [request] (home/index request))
-
   (GET "/new" [request] (game/newg request))
   (POST "/create" request (game/create request)) ;figure out how to destructure the origin url
-  (GET "/game/:id/join" [id :as request] (game-actions/join request id))
-  (POST "/game/:id/propose" [id :as request] (game-actions/propose request id))
   (GET "/game/:id" [id :as request] (game/show request id))
   (DELETE "/game/:id" [id :as request] (game/delete request id)))
+
+(defroutes game-action-routes
+  (GET "/game/:id/join" [id :as request] (game-actions/join request id))
+  (POST "/game/:id/propose" [id :as request] (game-actions/propose request id)))
 
 (defroutes login-routes
   (GET "/login" [request] (login/index request))
@@ -24,7 +25,8 @@
   (POST "/login" [name :as request] (login/create request name)))
 
 (defroutes main-routes
-  (auth/with-auth auth-routes)
+  (auth/with-auth resource-routes)
+  (auth/with-auth game-action-routes)
   (route/resources "/")
   (route/not-found "Not Found"))
 
